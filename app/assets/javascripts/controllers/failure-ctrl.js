833ae4617a7e339e26494314dcda3191c0fe1ddf
Parag Dadhaniya <paragdadhaniya@gmail.com>
2015-02-10 09:37:11 -0600

    add link to map

--- app/assets/javascripts/controllers/failure-ctrl.js
+++ app/assets/javascripts/controllers/failure-ctrl.js
@@ -3,17 +3,20 @@ app.controller('FailureCtrl', ['$scope', '$location', 'Failure',
     $scope.allFailures = Failure.allFailures;
     $scope.failureIndex = false;
     $scope.failureSingle = false;
+    $scope.googleMap = false;
     $scope.showAll = function() {
       $scope.failureIndex = true;
       $scope.failureSingle = false;
     };
     $scope.showRestaurant = function(restaurant) {
       $scope.singleRestaurant = restaurant;
-      // $scope.singleRestaurant.$promise.then(function(data) {
-        $scope.singleViolations = restaurant.violations;
-      // });
+      $scope.singleViolations = restaurant.violations;
       console.log($scope.singleRestaurant);
       $scope.failureIndex = false;
       $scope.failureSingle = true;
-    }
+    };
+    $scope.showMap = function(restaurant) {
+      console.log(restaurant);
+      $scope.googleMap = true;
+    };
   }]);
