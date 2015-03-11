97c4231e671309a72ee43c9bce3a37c877c7343e
Parag Dadhaniya <paragdadhaniya@gmail.com>
2015-03-10 20:56:04 -0500

    display name of restaurant after search by zip or date

--- app/assets/javascripts/controllers/zip-ctrl.js
+++ app/assets/javascripts/controllers/zip-ctrl.js
@@ -1,4 +1,5 @@
 app.controller('ZipCtrl', ['$scope', '$location', '$routeParams', 'Failure',
   function($scope, $location, $routeParams, Failure) {
     $scope.singleFailure = Failure.show({id: $routeParams["id"]});
+    $scope.searchFailure = true;
   }]);
