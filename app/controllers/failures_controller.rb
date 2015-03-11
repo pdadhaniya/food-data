865f4692d1a7bfb3ae60b56abfe4f9cba599dea8
Parag Dadhaniya <paragdadhaniya@gmail.com>
2015-03-10 21:14:16 -0500

    Refactor to remove class variables and unused arrays

--- app/controllers/failures_controller.rb
+++ app/controllers/failures_controller.rb
@@ -1,25 +1,32 @@
 class FailuresController < ApplicationController
   def index
-    @response = HTTParty.get('https://data.cityofchicago.org/resource/4ijn-s7e5.json?$limit=1000&results=Fail')
-    @complete_response = []
-    @response.each do |record|
-      @complete_response << record if record["violations"]
+    response = HTTParty.get('https://data.cityofchicago.org/resource/4ijn-s7e5.json?$limit=1000&results=Fail')
+    complete_response = []
+    response.each do |record|
+      complete_response << record if record["violations"]
     end
-    @proper_response = []
-    @complete_response.each do |record|
+    complete_response.each do |record|
       record["violations"] = record["violations"].titleize
       record["violations"] = record["violations"].split("|").sort
     end
-    render json: @complete_response
+    render json: complete_response
   end
 
   def show
     search = params[:id]
     if search.length == 5
-      @response = HTTParty.get("https://data.cityofchicago.org/resource/4ijn-s7e5.json?$limit=1000&results=Fail&zip="+search)
+      response = HTTParty.get("https://data.cityofchicago.org/resource/4ijn-s7e5.json?$limit=1000&results=Fail&zip="+search)
     elsif search.length == 10
-      @response = HTTParty.get("https://data.cityofchicago.org/resource/4ijn-s7e5.json?$limit=1000&results=Fail&inspection_date="+search+"T00:00:00")
+      response = HTTParty.get("https://data.cityofchicago.org/resource/4ijn-s7e5.json?$limit=1000&results=Fail&inspection_date="+search+"T00:00:00")
     end
-    render json: @response
+    complete_response = []
+    response.each do |record|
+      complete_response << record if record["violations"]
+    end
+    complete_response.each do |record|
+      record["violations"] = record["violations"].titleize
+      record["violations"] = record["violations"].split("|").sort
+    end
+    render json: response
   end
 end
