1a303f278847a0c430d553a20ef7eccb351f4449
Parag Dadhaniya <paragdadhaniya@gmail.com>
2015-04-19 12:27:45 -0500

    Split one violation between rule and comment

--- app/controllers/failures_controller.rb
+++ app/controllers/failures_controller.rb
@@ -6,12 +6,12 @@ class FailuresController < ApplicationController
       complete_response << record if record["violations"]
     end
     complete_response.each do |record|
-      record["violations"] = record["violations"].titleize
-      record["violations"] = record["violations"].split("|").sort
+      record["number"] = record["violations"].split("|").length
+      record["violations"] = record["violations"].titleize.split("|").sort.map { |x| x.split("   ") }.flatten
       record["dba_name"] = record["dba_name"].titleize
       record["address"] = "#{record["address"].chop}, #{record["city"]}, #{record["state"]} #{record["zip"]}"
     end
-    # binding.pry
+
     render json: complete_response
   end
 
